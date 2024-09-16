79e2c57754c9157a4429ace6f9a82bcbc8d0da8e
afza shaikh <shaikhafza961@gmail.com>
2024-09-16 22:16:33 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+"use strict";
+let mountains_list = [`k2`, `Mount everest`, `Margalla hills`, `Himalya`, `Manga parbat`];
+console.log(`\nList of mountains`);
+for (let i = 0; i < mountains_list.length; i++) {
+    console.log(`${mountains_list[i]}`);
+}
